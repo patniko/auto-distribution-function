0c39510ecd6bdfecc840b19fae7da79f408cb009
Patrick Nikoletich <pnikoletich@gmail.com>
2017-10-14 16:18:44 -0700

    Promises to fetch stats.

--- RereleaseTimer/index.js
+++ RereleaseTimer/index.js
@@ -1,31 +1,63 @@
-var request = require('request');
-
+var request = require('request-promise');
+var _ = require('lodash');
 //var momment = require('moment');
 //var mobileCenter = require('mobileCenter');
 
 module.exports = function (context, myTimer) {
     var timeStamp = new Date().toISOString();
-    
-    if(myTimer.isPastDue)
-    {
-        context.log('JavaScript is running late!');
-    }
 
+    var token = GetEnvironmentVariable("APP_CENTER_TOKEN");
+    var owner = GetEnvironmentVariable("APP_CENTER_OWNER");
+    var app = GetEnvironmentVariable("APP_CENTER_APP");
+
+    var sets = [{ "source": "Alpha Testers", "installs": 1, "sessions": 1, "crashes": 0, "destination": "Beta Testers" },
+                { "source": "Beta Testers", "installs": 2, "sessions": 2, "crashes": 0, "destination": "Google Play" }];
+    
     context.log(`Fetching the latest release version...`);
-    // curl -X GET "https://api.mobile.azure.com/v0.1/apps/Mobile-Center-Tester-Apps/MC-Tester-App-iOS/recent_releases" 
-    // -H "accept: application/json" -H "X-API-Token: fc4e0d7e807ba6161c720c851992deaeec566e75"
+    var options = BuildUrl("/recent_releases", token, owner, app);
+    request(options)
+    .then(response => {
+        context.log(response);
+        var latest_releases = JSON.parse(response);
 
-    context.log(`Fetching crash information for version...`);
-    //curl -X GET "https://api.mobile.azure.com/v0.1/apps/Mobile-Center-Tester-Apps/MC-Tester-App-iOS/analytics/crash_counts?start=2017-09-14" 
-    //-H "accept: application/json" -H "X-API-Token: fc4e0d7e807ba6161c720c851992deaeec566e75"
-    
-    context.log(`Fetching session averages for version...`);
-    //curl -X GET "https://api.mobile.azure.com/v0.1/apps/Mobile-Center-Tester-Apps/MC-Tester-App-iOS/analytics/session_durations_distribution?start=2017-09-14&versions=1.0.7" 
-    // -H "accept: application/json" -H "X-API-Token: fc4e0d7e807ba6161c720c851992deaeec566e75"
-    
-    context.log(`Fetching total installs for version...`);
-    // curl -X GET "https://api.mobile.azure.com/v0.1/apps/Mobile-Center-Tester-Apps/MC-Tester-App-iOS/analytics/versions?start=2017-08-01&versions=1.0.7" 
-    // -H "accept: application/json" -H "X-API-Token: fc4e0d7e807ba6161c720c851992deaeec566e75"
+        var crashes = new Promise((resolve, reject) => {
+            context.log(`Fetching crash information for version...`);
+            var options = BuildUrl("/analytics/crash_counts?start=2017-09-14&versions=1.0.7", token, owner, app);
+            request(options)
+            .then(results => { resolve(results); })
+            .error(response => {
+                context.error(response);
+            });
+        }); 
+
+        var sessions = new Promise((resolve, reject) => {
+            context.log(`Fetching session averages for version...`);
+            var options = BuildUrl("/analytics/session_durations_distribution?start=2017-09-14&versions=1.0.7", token, owner, app);
+            request(options)
+            .then(results => { resolve(results); })
+            .error(response => {
+                context.error(response);
+            });
+        }); 
+
+        var installs = new Promise((resolve, reject) => {
+            context.log(`Fetching total installs for version...`);
+            var options = BuildUrl("/analytics/versions?start=2017-08-01&versions=1.0.7", token, owner, app);
+            request(options)
+            .then(results => { resolve(results); })
+            .error(response => {
+                context.error(response);
+            });
+        }); 
+
+        Promise.all([crashes, sessions, installs]).then(values => { 
+            console.log(values); // [3, 1337, "foo"] 
+          });
+
+    })
+    .error(response => {
+        context.error(response);
+    });
 
     context.log(`Checking conditions...`);
 
@@ -35,8 +67,25 @@ module.exports = function (context, myTimer) {
     
     context.done();
 
-        /*request('http://www.google.com/', function(error, response, body) {
-        context.log("inside request callback");
-        context.done(); 
-    });*/
+
+
+
+    //request('http://www.google.com/', function(error, response, body) {
+    //    context.log("inside request callback");
+    //    context.done(); 
+    //});
+
 };
+
+function BuildUrl(endpoint, token, owner, app) {
+    var options = {
+        headers: { 'Accept': 'application/json', 'X-API-Token': token },
+        url: `https://api.mobile.azure.com/v0.1/apps/${owner}/${app}${endpoint}`
+    };
+    return options;
+}
+
+function GetEnvironmentVariable(name)
+{
+    return process.env[name];
+}
