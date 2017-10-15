b1e0b3664418a371a8d4b4a9cd68c55a87367034
Patrick Nikoletich <pnikoletich@gmail.com>
2017-10-15 16:30:07 -0700

    Fully working prototype minus re-release.

--- RereleaseTimer/index.js
+++ RereleaseTimer/index.js
@@ -1,91 +1,125 @@
 var request = require('request-promise');
 var _ = require('lodash');
-//var momment = require('moment');
-//var mobileCenter = require('mobileCenter');
 
 module.exports = function (context, myTimer) {
-    var timeStamp = new Date().toISOString();
+    const timeStamp = new Date().toISOString();
 
-    var token = GetEnvironmentVariable("APP_CENTER_TOKEN");
-    var owner = GetEnvironmentVariable("APP_CENTER_OWNER");
-    var app = GetEnvironmentVariable("APP_CENTER_APP");
+    const token = GetEnvironmentVariable("APP_CENTER_TOKEN");
+    const rules = require('./config.json');
 
-    var sets = [{ "source": "Alpha Testers", "installs": 1, "sessions": 1, "crashes": 0, "destination": "Beta Testers" },
-                { "source": "Beta Testers", "installs": 2, "sessions": 2, "crashes": 0, "destination": "Google Play" }];
+    for(rule of rules) 
+    {
+        const owner = rule.owner;
+        const app = rule.app;
+        const source = rule.source;
+        const destination = rule.destination;
+
+        context.log(`Processing rule for ${app} (${source} -> ${destination})...`);
+        var options = BuildUrl("/recent_releases", token, owner, app);
+        request(options) 
+        .then(response => {
+            var releases = JSON.parse(response);
+
+            let release = undefined;
+            for(i = 0; i < releases.length; i++) {
+                if(IsInGroup(releases[i], source)) {
+                    release = releases[i];
+                    break;
+                }
+            }
+
+            if(IsInGroup(release, destination))
+            {
+                context.log("Latest release has already been distributed to the destination.");
+                release = undefined;
+            }
+
+            if(release) {
+            var crashes = new Promise((resolve, reject) => {
+                context.log(`Fetching crash information for version...`);
+                var options = BuildUrl(`/analytics/crash_counts?start=${release.uploaded_at}&versions=${release.short_version}`, token, owner, app);
+                request(options)
+                .then(results => {
+                    results = JSON.parse(results);
+                    if(results.count)
+                        resolve(results.count);
+                    else
+                        resolve(0);
+                })
+                .error(response => {
+                    context.error(response);
+                });
+            }); 
     
-    context.log(`Fetching the latest release version...`);
-    var options = BuildUrl("/recent_releases", token, owner, app);
-    request(options)
-    .then(response => {
-        context.log(response);
-        var latest_releases = JSON.parse(response);
-
-        var crashes = new Promise((resolve, reject) => {
-            context.log(`Fetching crash information for version...`);
-            var options = BuildUrl("/analytics/crash_counts?start=2017-09-14&versions=1.0.7", token, owner, app);
-            request(options)
-            .then(results => { resolve(results); })
-            .error(response => {
-                context.error(response);
-            });
-        }); 
-
-        var sessions = new Promise((resolve, reject) => {
-            context.log(`Fetching session averages for version...`);
-            var options = BuildUrl("/analytics/session_durations_distribution?start=2017-09-14&versions=1.0.7", token, owner, app);
-            request(options)
-            .then(results => { resolve(results); })
-            .error(response => {
-                context.error(response);
-            });
-        }); 
-
-        var installs = new Promise((resolve, reject) => {
-            context.log(`Fetching total installs for version...`);
-            var options = BuildUrl("/analytics/versions?start=2017-08-01&versions=1.0.7", token, owner, app);
-            request(options)
-            .then(results => { resolve(results); })
-            .error(response => {
-                context.error(response);
-            });
-        }); 
-
-        Promise.all([crashes, sessions, installs]).then(values => { 
-            console.log(values); // [3, 1337, "foo"] 
-          });
-
-    })
-    .error(response => {
-        context.error(response);
-    });
-
-    context.log(`Checking conditions...`);
-
-    context.log(`Re-releasing latest version...`);
-
-    context.log('JavaScript timer trigger function ran!', timeStamp);   
+            var sessions = new Promise((resolve, reject) => {
+                context.log(`Fetching session averages for version...`);
+                var options = BuildUrl(`/analytics/session_durations_distribution?start=${release.uploaded_at}&versions=${release.short_version}`, token, owner, app);
+                request(options)
+                .then(results => {
+                    results = JSON.parse(results);
+                    if(results.distribution && results.distribution[2])
+                        resolve(results.distribution[2].count);
+                    else
+                        resolve(0);
+                })
+                .error(response => {
+                    context.error(response);
+                });
+            }); 
     
-    context.done();
+            var installs = new Promise((resolve, reject) => {
+                context.log(`Fetching total installs for version...`);
+                var options = BuildUrl(`/analytics/versions?start=${release.uploaded_at}&versions=${release.short_version}`, token, owner, app);
+                request(options)
+                .then(results => {
+                    results = JSON.parse(results);
+                    if (results.versions && results.versions[0])
+                        resolve(results.versions[0].count);
+                    else
+                        resolve(0);
+                })
+                .error(response => {
+                    context.error(response);
+                });
+            }); 
+    
+            Promise.all([crashes, sessions, installs]).then(values => { 
+                
+                context.log(`Checking conditions...`);
+                if (crashes <= rule.crashes &&
+                    installs >= rule.installs &&
+                    sessions >= rule.sessions) {
 
-
-
-
-    //request('http://www.google.com/', function(error, response, body) {
-    //    context.log("inside request callback");
-    //    context.done(); 
-    //});
-
+                        context.log(`Re-releasing latest version...`);
+                }
+              });
+            }
+        })
+        .error(response => {
+            context.error(response);
+        });
+    }
 };
 
+function GetEnvironmentVariable(name)
+{
+    return process.env[name];
+}
+
 function BuildUrl(endpoint, token, owner, app) {
-    var options = {
+    const options = {
         headers: { 'Accept': 'application/json', 'X-API-Token': token },
         url: `https://api.mobile.azure.com/v0.1/apps/${owner}/${app}${endpoint}`
     };
     return options;
 }
 
-function GetEnvironmentVariable(name)
-{
-    return process.env[name];
+function IsInGroup(release, group) {
+    if(release.distribution_groups) {
+        for(i = 0; i < release.distribution_groups.length; i++) {
+            if(release.distribution_groups[i].name == group)
+                return true;
+        }
+    }
+    return false;
 }
